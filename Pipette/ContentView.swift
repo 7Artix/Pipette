7d9028fb3d1d720acda406fb38bed497be1f7976
Artix <artixzhang@icloud.com>
2025-07-16 22:45:18 +0800

    Initial Commit

---
+++ Pipette/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pipette
+//
+//  Created by Artix Zhang on 2025/7/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pipette/PipetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipetteApp.swift
+//  Pipette
+//
+//  Created by Artix Zhang on 2025/7/16.
+//
+
+import SwiftUI
+
+@main
+struct PipetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
